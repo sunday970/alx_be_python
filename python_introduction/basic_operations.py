9cdac8e3dd4b2f1a6d8445698a5e6e4498853570
sunday owuru <sundayowuru9@gmail.com>
2024-09-16 14:54:52 +0100

    simple addition, subtraction and multiplication

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -2,17 +2,16 @@ number1=10
 number2 =5
 
 print (f"Addition of {number1} and {number2} is {number1 + number2}")
-number1=10
-number2=5
+
 
 
 print (f"subtraction of {number1} and {number2} is {number1 - number2}")
-number1=10
-number2=5
+
+
 
 print (f"multiplication of {number1} and {number2} is {number1 * number2}")
-number1=10
-number2=5
+
+
 
 
 
