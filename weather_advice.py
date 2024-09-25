5f7d3c52ab8458e1faef444f13cf05a771448256
sunday owuru <sundayowuru9@gmail.com>
2024-09-25 13:19:07 +0100

    code successful

---
+++ weather_advice.py
@@ -0,0 +1,17 @@
+sunday owuru (15):
+      simple addition, subtraction and multiplication
+      simple addition, subtraction and multiplication
+      simple interest calculator
+      simple interest calculator
+      area of rectangle sorted
+      simple adition, multiplication and subtraction
+      simple adition, multiplication and subtraction
+      simple interest calculated
+      rectangle calculated
+      hours to seconds calculated
+      hours to seconds calulated
+      hours to seconds calculated
+      future age calculated
+      future_age calculated
+      done message
+
